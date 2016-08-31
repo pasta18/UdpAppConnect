c1cff2ca6c8b79b065a04572b8aef3b2ad3d7035
pasta18 <lightsworn449black@gmal.com>
2016-08-31 18:19:58 +0900

    update

---
+++ Assets/Scripts/TestUdp.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Net;
+using System;
+
+public class TestUdp : MonoBehaviour
+{
+    UdpDataClient udp;
+
+    [SerializeField]
+    Button send;
+    [SerializeField]
+    Button receive;
+    [SerializeField]
+    Text text;
+    [SerializeField]
+    Button textSend;
+
+    [SerializeField]
+    Text[] Address;
+
+    [SerializeField]
+    Button AddressButton;
+    [SerializeField]
+    Text AddressButtonText;
+
+    private bool AddressFlag = false;
+    private string ipAddress;
+
+    public void Start()
+    {
+        AddressButton.interactable = false;
+    }
+
+    public void Update()
+    {
+        if (Address.Length == 4)
+        {
+            try
+            {
+                foreach (var address in Address)
+                {
+                    if (address.text.Length <= 0 || int.Parse(address.text) > 255 || int.Parse(address.text) < 0)
+                    {
+                        AddressButton.interactable = false;
+                        return;
+                    }
+                }
+            }
+            catch(FormatException e)
+            {
+                Debug.Log("記号はNG");
+                return;
+            }
+            AddressButton.interactable = true;
+        }
+    }
+
+    public void SendButtonSet()
+    {
+        if (AddressFlag)
+        {
+            Debug.Log(ipAddress);
+            udp = new UdpDataClient(false, ipAddress);
+        }
+        else udp = new UdpDataClient(false);
+        send.interactable = false;
+        receive.interactable = false;
+        textSend.interactable = true;
+    }
+
+    public void ReceiveButtonSet()
+    {
+        udp = new UdpDataClient(true);
+        send.interactable = false;
+        receive.interactable = false;
+        udp.AddReceiveEvent(OnReceive);
+    }
+
+    public void AddressSetButton()
+    {
+        AddressFlag = !AddressFlag;
+
+        if (AddressFlag)
+        {
+            AddressButtonText.text = "Cancel";
+            SetAddress();
+        }
+        else
+        {
+            AddressButtonText.text = "Set";
+        }
+
+        foreach(var address in Address)
+        {
+            var inputField = address.GetComponent<InputField>();
+
+            if(inputField != null)
+            {
+                address.GetComponent<InputField>().interactable = !inputField.interactable;
+            }
+        }
+    }
+
+    public void TextSendButtonClick()
+    {
+        udp.Send(text.text);
+    }
+
+    public void OnApplicationQuit()
+    {
+        if (udp != null) udp.EndSocket();
+    }
+
+    public void OnReceive(string data)
+    {
+        Debug.Log(data);
+    }
+
+    public void SetAddress()
+    {
+        ipAddress = "";
+        ipAddress += Address[0].text;
+
+        for(int i = 1; i < Address.Length;i++)
+        {
+            ipAddress += "." + Address[i].text;
+        }
+    }
+}
---
+++ Assets/Scripts/UdpDataClient.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text;
+using System;
+
+public class UdpDataClient
+{
+    public readonly bool isServer; // サーバー側ならtrue
+    public bool isRunningWork; // 動作中ならtrue
+    public bool isSend = false; // 送信ボタンが押されたらtrue
+
+    private readonly int PACKET_SIZE;
+    private byte[] packet; // データを確認
+
+    private Thread runWorkThread;
+
+    private Socket sock;
+
+    private event Action<String> onReceive = delegate { };
+
+
+    public UdpDataClient(bool isServer, string address = null, int packetSize = 1024)
+    {
+        //Debug.Log("Create");
+        this.isServer = isServer;
+        PACKET_SIZE = packetSize;
+        packet = new byte[PACKET_SIZE];
+
+        sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        RunWorkAsync(8000, address);
+    }
+
+    // 非同期通信の開始
+    public void RunWorkAsync(int port, string stringAddress = null)
+    {
+        isRunningWork = true;
+
+        IPAddress ipAddress = null;
+
+        if(stringAddress != null) ipAddress = IPAddress.Parse(stringAddress);
+
+        //Debug.Log("RunWork");
+
+        if (isServer) // サーバー側
+        {
+            runWorkThread = new Thread(new ThreadStart(() =>
+            {
+                var local = new IPEndPoint(IPAddress.Any, port);
+                var remote = new IPEndPoint(IPAddress.Any, port) as EndPoint;
+                sock.Bind(local);
+
+                Debug.Log("Server");
+
+                while (isRunningWork)
+                {
+                    Debug.Log("Run");
+                    var length = sock.ReceiveFrom(packet, ref remote);
+                    var data = Encoding.UTF8.GetString(packet);
+
+                    this.OnReceive(data);
+                }
+            }));
+        }
+        else // クライアント側
+        {
+            runWorkThread = new Thread(new ThreadStart(() =>
+            {
+                IPAddress address;
+
+                if (ipAddress == null) address = IPAddress.Loopback;
+                else
+                {
+                    address = ipAddress;
+                }
+                var remote = new IPEndPoint(address, port);
+
+                sock.Connect("localhost", port);
+
+                Debug.Log("Client");
+
+                while (isRunningWork)
+                {
+                    if(isSend)
+                    {
+                        Debug.Log("Send");
+                        sock.SendTo(packet, remote);
+                        isSend = false;
+                    }
+                }
+            }));
+        }
+
+        runWorkThread.Start();
+    }
+    // 受け取ったときの動作
+    private void OnReceive(string data)
+    {
+        onReceive(data);
+    }
+    // 受け取ったときの動作の追加
+    public void AddReceiveEvent(Action<string> action)
+    {
+        onReceive += action;
+    }
+    // 通信終了
+    public void EndSocket()
+    {
+        if (isRunningWork)
+        {
+            isRunningWork = false;
+
+            if (!isServer)
+            {
+                runWorkThread.Join();
+            }
+            else
+            {
+                UdpDataClient udp = new UdpDataClient(false);
+                udp.Send("Exit");
+                udp.EndSocket();
+            }
+        }
+        if (sock != null)
+        {
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
+        }
+    }
+    // 送信の準備
+    public void Send(string data)
+    {
+        packet = Encoding.UTF8.GetBytes(data);
+        isSend = true;
+    }
+}
